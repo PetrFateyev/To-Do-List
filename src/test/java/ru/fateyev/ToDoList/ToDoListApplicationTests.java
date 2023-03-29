0df5350c77ec83c0541c80ccf34b7c944ae07ccc
Petr <fateyevpetr@gmail.com>
2023-03-29 17:47:04 +0700

    first commit

---
+++ src/test/java/ru/fateyev/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fateyev.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
