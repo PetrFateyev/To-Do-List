8b795c508bbfb65a069b15ede41d0df0879129d7
Petr <fateyevpetr@gmail.com>
2023-07-23 15:30:43 +0700

    add enum for sort selection

---
+++ src/main/java/ru/fateyev/ToDoList/controllers/ListSort.java
@@ -0,0 +1,20 @@
+package ru.fateyev.ToDoList.controllers;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+
+@Getter
+@RequiredArgsConstructor
+public enum ListSort {
+
+    ID_ASC(Sort.by(Sort.Direction.ASC, "id")),
+    NAME_ASC(Sort.by(Sort.Direction.ASC, "name")),
+    NAME_DESC(Sort.by(Sort.Direction.DESC, "name")),
+    CREATED_DATE_ASC(Sort.by(Sort.Direction.ASC, "created_date")),
+    CREATED_DATE_DESC(Sort.by(Sort.Direction.DESC, "created_date")),
+    MODIFIED_DATE_ASC(Sort.by(Sort.Direction.ASC, "modified_date")),
+    MODIFIED_DATE_DESC(Sort.by(Sort.Direction.DESC, "modified_date"));
+
+    private final Sort sortValue;
+}
