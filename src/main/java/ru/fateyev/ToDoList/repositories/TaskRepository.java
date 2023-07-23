66430341bb189b70b16968a7e66a83adeae91ff5
Petr <fateyevpetr@gmail.com>
2023-07-23 13:02:51 +0700

    add methods: findByToDoListId, deleteByToDoListId

--- src/main/java/ru/fateyev/ToDoList/repositories/TaskRepository.java
+++ src/main/java/ru/fateyev/ToDoList/repositories/TaskRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.fateyev.ToDoList.models.Task;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    List<Task> findByToDoListId(int listId);
+    void deleteByToDoListId(int listId);
 }
