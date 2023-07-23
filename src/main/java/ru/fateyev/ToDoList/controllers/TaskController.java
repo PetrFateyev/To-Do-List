c85aec53125b68c838b8c03dce4a23213b0e3676
Petr <fateyevpetr@gmail.com>
2023-07-23 15:36:14 +0700

    change methods: getTask, createTask add methods: getAllTaskByToDoListId, markDone, deleteAll. delete methods: getTasks.

--- src/main/java/ru/fateyev/ToDoList/controllers/TaskController.java
+++ src/main/java/ru/fateyev/ToDoList/controllers/TaskController.java
@@ -3,7 +3,6 @@ package ru.fateyev.ToDoList.controllers;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.fateyev.ToDoList.models.Task;
@@ -11,7 +10,7 @@ import ru.fateyev.ToDoList.services.TaskService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/task")
+@RequestMapping("/api")
 public class TaskController {
 
     private final TaskService taskService;
@@ -21,37 +20,52 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    //Метод для получения всех дел
-    @GetMapping
-    public List<Task> getTasks(){
-        return taskService.findAll();
+    //Метод для получения всех дел в списке
+    @GetMapping("/lists/{listId}/tasks")
+    public ResponseEntity<List<Task>> getAllTaskByToDoListId(@PathVariable(value = "listId") int id) {
+        return new ResponseEntity<>(taskService.findByToDoListId(id), HttpStatus.OK);
     }
 
     //Метод для получения дела по id
-    @GetMapping("/{id}")
-    public Task getTask(@PathVariable("id") int id){
-        return taskService.findOne(id);
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable("id") int id){
+        return new ResponseEntity<>(taskService.findOne(id), HttpStatus.OK);
     }
 
-    //Метод для добавления дела
-    @PostMapping
-    public ResponseEntity<HttpStatus> createTask(@RequestBody @Valid Task task){
-        taskService.save(task);
+
+    //Метод для добавления дела в список
+    @PostMapping("/lists/{listId}/tasks")
+    public ResponseEntity<HttpStatus> createTask(@PathVariable(value = "listId") int listId, @RequestBody @Valid Task task){
+        taskService.create(listId,task);
         return ResponseEntity.ok(HttpStatus.CREATED);
     }
 
-    //Метод для измениния дела
-    @PutMapping(value = "/{id}")
+    //Метод для изменения дела
+    @PutMapping(value = "/tasks/{id}")
     public ResponseEntity<HttpStatus> updateTask(@PathVariable("id") int id, @RequestBody @Valid Task task) {
         taskService.update(id, task);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    //Метод чтобы пометить дело, как сделанное
+    @PutMapping("/tasks/mark-done/{id}")
+    public ResponseEntity<HttpStatus> markDone(@PathVariable("id") int id, @RequestBody @Valid Task task) {
+        taskService.markDone(id, task);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
     //Метод для удаления дела
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/tasks/{id}")
     public ResponseEntity<HttpStatus> deleteById(@PathVariable("id") int id){
         taskService.delete(id);
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    //Метод для удаления всех дел в списке
+    @DeleteMapping("/lists/listId/tasks")
+    public ResponseEntity<HttpStatus> deleteAll(@PathVariable("listId") int id){
+        taskService.deleteAll(id);
+        return ResponseEntity.ok(HttpStatus.NO_CONTENT);
     }
 
 }
