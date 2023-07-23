d91e80c7f15df1a08558ed163e0279e0042408bd
Petr <fateyevpetr@gmail.com>
2023-07-23 15:25:56 +0700

    change methods: save, update. add methods: markDone, deleteAll, findByToDoListId

--- src/main/java/ru/fateyev/ToDoList/services/TaskService.java
+++ src/main/java/ru/fateyev/ToDoList/services/TaskService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.fateyev.ToDoList.models.Task;
 import ru.fateyev.ToDoList.repositories.TaskRepository;
+import ru.fateyev.ToDoList.repositories.ToDoListRepository;
 import ru.fateyev.ToDoList.util.NotFoundException;
 import java.util.List;
 
@@ -13,29 +14,51 @@ import java.util.List;
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final ToDoListRepository toDoListRepository;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, ToDoListRepository toDoListRepository) {
         this.taskRepository = taskRepository;
+        this.toDoListRepository = toDoListRepository;
     }
 
     @Transactional
-    public void save(Task task){
-        taskRepository.save(task);
+    public void create(int listId, Task task) {
+        toDoListRepository.findById(listId).map(list -> {
+            task.setToDoList(list);
+            return taskRepository.save(task);
+        }).orElseThrow(() -> new NotFoundException("List with this id was not found"));
     }
 
-    public List<Task> findAll(){
-        return taskRepository.findAll();
+    public List<Task> findByToDoListId(int listId) {
+        if (!toDoListRepository.existsById(listId)) {
+            throw new NotFoundException("List with this id was not found");
+        }
+        return taskRepository.findByToDoListId(listId);
     }
 
     public Task findOne(int id){
-        return taskRepository.findById(id).orElseThrow(()-> new NotFoundException("Task with this id was not found"));
+        return taskRepository.findById(id)
+                .orElseThrow(()-> new NotFoundException("Task with this id was not found"));
     }
 
     @Transactional
     public void update(int id, Task updatedTask){
+        if (!taskRepository.existsById(id)) {
+            throw new NotFoundException("Task with this id was not found");
+        }
         updatedTask.setId(id);
         taskRepository.save(updatedTask);
+    }
+
+    @Transactional
+    public void markDone(int id, Task task) {
+        if (!taskRepository.existsById(id)) {
+            throw new NotFoundException("Task with this id was not found");
+        }
+        task.setId(id);
+        task.setDone(true);
+        taskRepository.save(task);
     }
 
     @Transactional
@@ -43,4 +66,11 @@ public class TaskService {
         taskRepository.deleteById(id);
     }
 
+    @Transactional
+    public void deleteAll(int listId) {
+        if (!toDoListRepository.existsById(listId)) {
+            throw new NotFoundException("List with this id was not found");
+        }
+        taskRepository.deleteByToDoListId(listId);
+    }
 }
